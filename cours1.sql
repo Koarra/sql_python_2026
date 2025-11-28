273b769808a5705f6911e7c34c6e2eb053d6d5d6
montemerro <firat_erman@outlook.com>
2025-11-28 12:12:03 +0100

    premiere seance

---
+++ cours1.sql
@@ -0,0 +1,72 @@
+/* step 1 : Create table about our customers and their cars */
+DROP TABLE IF EXISTS sql_2026.clients;
+CREATE TABLE sql_2026.clients
+( 
+nom VARCHAR(30),
+prenom VARCHAR(30),
+age INT,
+ville VARCHAR(30),
+marque VARCHAR(20)
+);
+
+
+/*step 2 : add data to the table*/
+
+INSERT INTO sql_2026.clients (nom, prenom, age, ville, country)
+VALUES ("Musk","Elon",50,"miami","usa"),
+("Mbappe","Kylian",23,"Paris","france"),
+("Bryant","Kobe",41,"Los angeles","usa");
+
+
+/* pour selectionner toutes les colonnes */
+SELECT 
+* 
+FROM sql_2026.clients;
+
+/* pour selectionner uniquement deux colonnes: nom et prenom   */
+SELECT 
+	nom,
+    prenom
+FROM sql_2026.clients;
+
+
+/* ==============
+OPERATIONS SUR DES COLONNES
+============== */
+/* compter le nombre total de lignes de ma table: command COUNT*/
+SELECT 
+	COUNT(*)
+FROM sql_2026.clients;
+
+/* afficher tous les elements uniques d'une colonne, ici on prend la colonne pays: command DISTINCT*/
+SELECT
+	DISTINCT(pays)
+FROM sql_2026.clients;
+
+/* compter le nombre unique de pays */
+SELECT
+	 COUNT(DISTINCT(pays))
+FROM sql_2026.clients;
+
+
+/*creer une table a partir d'une autre table*/
+
+CREATE TABLE sql_2026.clients2 AS 
+SELECT 
+	*
+FROM sql_2026.clients;
+
+/*ajout de nouvelles colonnes*/
+SELECT 
+	*,
+    pays AS pays2,
+    age+10 AS age_in_2035
+FROM sql_2026.clients;
+
+
+/*ajout de nouvelles colonnes*/
+SELECT 
+	*,
+    pays AS pays2,
+    age+10 AS age_in_2035
+FROM sql_2026.clients;
